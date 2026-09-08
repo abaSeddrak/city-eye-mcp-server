904ccdabb9fb271b88c8e43dd63a4415135c725e
abanoub <abanoub.anwar@sprinteye.com>
2026-09-08 11:12:40 +0300

    Fix allowed_hosts with actual IP wildcard

--- mcp_instance.py
+++ mcp_instance.py
@@ -4,7 +4,6 @@ from mcp.server.transport_security import TransportSecuritySettings
 mcp = FastMCP(
     "city_eye",
     transport_security=TransportSecuritySettings(
-        allowed_hosts=["*"],
-        allowed_origins=["*"],
+        allowed_hosts=["35.87.188.43:*", "localhost:*", "127.0.0.1:*"],
     ),
 )
